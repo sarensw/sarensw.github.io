ceac5131fc8e7683d88f19618187043b090a4668
Stephan Arenswald <stephan@sarensw.com>
2020-04-02 14:58:31 +0200

    Added mobile support

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
         header: ["Saira Extra Condensed", ...defaultTheme.fontFamily.sans]
+      },
+      height: {
+        96: "24rem",
+        "500px": "500px",
+        "382px": "382px"
+      },
+      width: {
+        "1024px": "1024px",
+        788: "788px"
       }
     },
   },
